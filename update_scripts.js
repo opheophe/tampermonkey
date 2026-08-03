56bcfa86f79f15859864c8592d771a4df9a5b4a8
Ophe <opheophe@gmail.com>
2026-08-03 08:53:44 +0200

    Update script metadata and requirements
    Removed outdated metadata and consolidated script requirements.

---
+++ update_scripts.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         All my Scripts
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Installs and runs all my Tampermonkey scripts at once!
+// @author       You
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/opheophe/tampermonkey/main/update_scripts.js
+// @downloadURL  https://raw.githubusercontent.com/opheophe/tampermonkey/main/update_scripts.js
+
+// @require      https://raw.githubusercontent.com/opheophe/tampermonkey/main/update_scripts.js
+// @require      https://raw.githubusercontent.com/opheophe/tampermonkey/main/picpac_export_av_enkel_tabell.js
+// @require      https://raw.githubusercontent.com/opheophe/tampermonkey/main/picpac_fetch_lp_rec_date.js
+// @require      https://raw.githubusercontent.com/opheophe/tampermonkey/main/picpac_page_dropdown.js
+
+
+// ==/UserScript==
+
+// Leave the body empty! The @require tags handle loading and executing the code.
+
