4250bd269a9105ead1b55da63da95cee8001a321
Ophe <opheophe@gmail.com>
2026-08-03 08:43:50 +0200

    Update script version and URLs in metadata

--- picpac_export_av_enkel_tabell.js
+++ picpac_export_av_enkel_tabell.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         PicPac - Universal simple table-fetch
 // @namespace    http://tampermonkey.net/
-// @version      2026-07-22
+// @version      1.0.1
 // @description  Fetches all pages sequentially for static tables and downloads CSV
 // @author       Cristopher Dahlström
 // @match        https://picpac.medovia.se/*
 // @run-at       document-idle
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/opheophe/tampermonkey/main/picpac_export_av_enkel_tabell.js
+// @downloadURL  https://raw.githubusercontent.com/opheophe/tampermonkey/main/picpac_export_av_enkel_tabell.js
 // ==/UserScript==
 
 (function() {
