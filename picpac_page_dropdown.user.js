ab6dd6c5e9699ef3da631d26774b788ed325afb5
Ophe <opheophe@gmail.com>
2026-08-13 10:13:46 +0200

    Update version and change dropdown value to 1000000

--- picpac_page_dropdown.user.js
+++ picpac_page_dropdown.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PicPac - Default x Rows (Universal)
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Forcibly sets rows per page dropdown to x on all Medovia PicPac datatables
 // @author       Cristopher Dahlström
 // @match        https://picpac.medovia.se/*
@@ -32,20 +32,20 @@
     }, 10000);
 
     function setDropdownTo200(selectElement) {
-        if (selectElement.value === '200') return;
+        if (selectElement.value === '1000000') return;
 
         // Verify if 200 is present in the list; if not, inject it
-        let optionExists = Array.from(selectElement.options).some(opt => opt.value === '200');
+        let optionExists = Array.from(selectElement.options).some(opt => opt.value === '1000000');
 
         if (!optionExists) {
             const newOption = document.createElement('option');
-            newOption.value = '200';
-            newOption.text = '200';
+            newOption.value = '1000000';
+            newOption.text = '1000000';
             selectElement.add(newOption);
         }
 
         // Apply change
-        selectElement.value = '200';
+        selectElement.value = '1000000';
 
         // Trigger both change and input events so AngularJS registers the update and reloads the table data
         const changeEvent = new Event('change', { bubbles: true });
@@ -54,6 +54,6 @@
         const inputEvent = new Event('input', { bubbles: true });
         selectElement.dispatchEvent(inputEvent);
 
-        console.log('Tampermonkey: Successfully defaulted table limit to 200 rows.');
+        console.log('Tampermonkey: Successfully defaulted table limit to 1000000 rows.');
     }
 })();
